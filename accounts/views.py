e00c027c1959c8b1e047ac4c5c79638c8c02d900
pra17dod <pra17dod@gmail.com>
2021-03-25 09:57:27 +0530

    Add accounts app

---
+++ accounts/views.py
@@ -0,0 +1,44 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
+
+import os
+
+# Create your views here.
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Your Account is created! Try Login!')
+            return redirect('login')
+        else: 
+            messages.error(request, f'Invalid input')
+
+    else:
+        form = UserRegisterForm()
+
+    return render(request, 'accounts/register.html', {'form' : form})
+
+@login_required
+def profile(request):
+    if request.method == 'POST':
+        u_form = UserUpdateForm(request.POST, instance= request.user)
+        p_form = ProfileUpdateForm(request.POST, request.FILES, instance = request.user.profile)
+        # to delete old image on update
+        old_pic = request.user.profile.image.path
+        if u_form.is_valid and p_form.is_valid:
+            u_form.save()
+            p_form.save()
+            # os.remove(old_pic)
+            messages.success(request, f'Profile updated')
+            return redirect('profile')
+    else:
+        u_form = UserUpdateForm(instance= request.user)
+        p_form = ProfileUpdateForm(instance = request.user.profile)
+
+    ctx = { 'u_form': u_form, 'p_form': p_form }
+    return render(request, 'accounts/profile.html', ctx)
