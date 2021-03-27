a984b270411ab2601545dd8fe4329ba999d1c0ef
pra17dod <pra17dod@gmail.com>
2021-03-27 10:28:52 +0530

    Add views for Rest API func based

--- blog/views.py
+++ blog/views.py
@@ -3,8 +3,57 @@ from .models import Post
 from django.contrib.auth.models import User
 from django.views.generic import *
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
-# Create your views here.
 
+### Rest API imports
+from django.http import HttpResponse, JsonResponse
+from rest_framework.parsers import JSONParser
+from .serializers import PostSerializer
+from django.views.decorators.csrf import csrf_exempt
+
+### Rest API Function Based Views
+
+@csrf_exempt
+def post_list(request):
+
+    if request.method == 'GET':
+        posts = Post.objects.all()
+        serializer = PostSerializer(posts, many=True)
+        return JsonResponse(serializer.data, safe = False)
+
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = PostSerializer(data = data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        
+        return JsonResponse(serializer.error, status=400)
+
+@csrf_exempt
+def post_detail(request, pk):
+    try:
+        post = Post.objects.get(pk=pk)
+    except Post.DoesNotExist:
+        return HttpResponse(status=404)
+
+    if request.method == 'GET':
+        serializer = PostSerializer(post)
+        return JsonResponse(serializer.data)
+
+    elif request.method == 'PUT':
+        data = JSONParser().parse(request)
+        serializer = PostSerializer(post, data= data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data)
+        return JsonResponse(serializer.errors, status=400)
+
+    elif request.method == 'DELETE':
+        post.delete()
+        return HttpResponse(status=204)
+
+
+### Function Based Views
 def home(request):
     posts = Post.objects.all()
     ctx = { 'posts': posts }
@@ -13,6 +62,7 @@ def home(request):
 def about(request):
     return render (request, 'blog/about.html', {'title': 'About'})
 
+### Class Based Views
 class PostListView(ListView):
     model = Post
     # template_name = 'blog/home.html'
