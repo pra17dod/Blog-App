59a68cf12bb617e89bf0b6e23aadb0b78f01c0ea
pra17dod <pra17dod@gmail.com>
2021-03-27 11:07:31 +0530

    Set fields to all

--- blog/serializers.py
+++ blog/serializers.py
@@ -5,6 +5,6 @@ class PostSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Post
-        fields = [ 'id', 'title', 'content', 'author', 'created_at']
-    
+        # fields = [ 'id', 'title', 'content', 'author', 'created_at']
+        fields = '__all__'
 
